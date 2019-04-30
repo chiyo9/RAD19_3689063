2f52ab6cc3370397b4bbbadd62c29ec8d82b6b52
Tony Jiang <xsjcTony@126.com>
2019-04-30 12:07:39 +0000

    finish setting up sign up page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get '/home',                to: 'static_pages#home'
+  get '/home',    to: 'static_pages#home'
   get '/courses', to: 'static_pages#courses'
+  get 'signup',   to: 'users#new'
 end
