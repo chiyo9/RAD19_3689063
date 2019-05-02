b93a84732aa21e49671af4b7da20fe09ddc9490e
Tony Jiang <xsjcTony@126.com>
2019-05-02 16:02:58 +0000

    finish sign up and all regarding tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/home',    to: 'static_pages#home'
   get '/courses', to: 'static_pages#courses'
   get '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
