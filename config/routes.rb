7f41242fc667dd7fbbf049a2104ef322203cf3fd
Tony Jiang <xsjcTony@126.com>
2019-05-02 15:33:20 +0000

    Finish modeling User (incl. secure-password, all tests)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
   get '/home',    to: 'static_pages#home'
   get '/courses', to: 'static_pages#courses'
-  get '/signup',   to: 'users#new'
+  get '/signup',  to: 'users#new'
 end
